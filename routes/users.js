2b64f355b3837b6c4aeb1d9fa26af6af2255ae1a
fairlyxu <feilibaby@163.com>
2018-03-15 12:50:01 +0800

    add default

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,16 @@
 var express = require('express');
 var router = express.Router();
-
+var mid = require('./middleware');
  
 /* GET users profile. */
+
 router.get('/profile', function(req, res, next) {
-  res.render('profile', { title: '用户' });
+	var url = "/user_mgmt/user_info?userId=1";
+	mid.find(req, url, function(data){  
+		res.render('profile',{user:data.data} );
+		//res.end();
+  }) 
+ 
 });
 
 module.exports = router;
